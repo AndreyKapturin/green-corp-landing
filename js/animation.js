924a10da15daa4ed1aebd8ae9459b50e4170c9ce
AndreyKapturin <andruxa21097@gmail.com>
2023-03-08 21:50:23 +0700

    Добавил анимацию скролла

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,5 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 15;
+let animationInited = false;
 
 function increaseNumberAnimationStep(i, element, endNumber) {
     if (i <= endNumber) {
@@ -16,8 +17,6 @@ function initIncreaseNumberAnimation() {
     const element = document.querySelector(".features__clients-count");
     increaseNumberAnimationStep(0, element, 5000);
 }
-
-  initIncreaseNumberAnimation();
 
 
 document.querySelector('#budget').addEventListener("change", function handleSelectChange(event) {
@@ -40,3 +39,39 @@ document.querySelector('#budget').addEventListener("change", function handleSele
         document.querySelector("#form form").removeChild(otherInput)
     }
 })
+
+
+function updateScroll() {
+    const header = document.querySelector("header")
+    if (window.scrollY === 0){
+        header.classList.remove("header__scrolled")
+    } else {
+        header.classList.add("header__scrolled")
+    }
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+        initIncreaseNumberAnimation()
+        animationInited = true
+    }
+}
+
+window.addEventListener("scroll", updateScroll)
+
+function addSmoothScroll(link) {
+    link.addEventListener("click", onLinkClick)
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    document.querySelector(event.target.getAttribute("href")).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.querySelectorAll("a[href^='#']").forEach(link => {
+    addSmoothScroll(link)
+})
+
+addSmoothScroll(document.querySelector(".more-button"))
+console.log(document.querySelector("#form").parentElement);
